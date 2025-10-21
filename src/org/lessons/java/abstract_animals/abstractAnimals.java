b95e9c46e833654900f3608765c0a55f9d052227
Francesco Sales <frankds1997@gmail.com>
2025-10-21 17:20:22 +0200

    definiti i metodi nella classe astratta

--- src/org/lessons/java/abstract_animals/abstractAnimals.java
+++ src/org/lessons/java/abstract_animals/abstractAnimals.java
@@ -2,5 +2,15 @@ package org.lessons.java.abstract_animals;
 
 public class abstractAnimals {
 
-    
+    protected void dormi(){
+        System.out.println("zzzzz");
+    }
+
+    protected void verso(){
+
+    }
+
+    protected void mangia(){
+        
+    }
 }
